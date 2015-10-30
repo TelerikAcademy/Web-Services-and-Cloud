6973be023c46a0fdb4f9e52617e36a8074d9fd1c
Nikolay Kostov <admin@nikolay.it>
2015-10-30 11:47:00 +0200

    Added LiveDemo-2015-SourceControlSystem

---
+++ 3. ASP.NET-Web-API-Architecture/LiveDemo-2015-SourceControlSystem/Server/SourceControlSystem.Api/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿namespace SourceControlSystem.Api
+{
+    using Common.Constants;
+    using System.Reflection;
+    using System.Web;
+    using System.Web.Http;
+
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            DatabaseConfig.Initialize();
+            AutoMapperConfig.RegisterMappings(Assembly.Load(Assemblies.WebApi));
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
